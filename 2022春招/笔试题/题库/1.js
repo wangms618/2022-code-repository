3e3cd680ac15c0990d76548368a63e192b039564
王铭顺 <553147551@qq.com>
2022-02-21 22:50:33 +0800

    笔试

---
+++ 2022春招/笔试题/题库/1.js
@@ -0,0 +1,5 @@
+const func = () => {}
+const obj = {}
+const str = ''
+console.log(typeof func, typeof obj, typeof str, typeof null);
+// function object string object
---
+++ 2022春招/笔试题/题库/2.js
@@ -0,0 +1,8 @@
+console.log(typeof {
+    "x": 1
+});
+console.log(typeof 1);
+console.log(typeof [{
+    x: 1
+}]);
+console.log(typeof NaN);
