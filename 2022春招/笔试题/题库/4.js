5d82d77fad1d18d1f5e1e72967b627e5cfc9cf75
王铭顺 <553147551@qq.com>
2022-02-22 22:53:25 +0800

    笔试题

---
+++ 2022春招/笔试题/题库/4.js
@@ -0,0 +1,7 @@
+const fn = function () {}
+console.log(JSON.stringify(fn));
+const num = 123
+console.log(JSON.stringify(num));
+const b = true
+console.log(JSON.stringify(b));
+console.log(JSON.stringify(NaN));
---
+++ 2022春招/笔试题/题库/5.js
@@ -0,0 +1,5 @@
+const arr = [1, 2, 3]
+const arr2 = [4]
+
+arr.sort((a, b) => b - a)
+console.log(arr); // 会改变原数组
