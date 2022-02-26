e7498e3feec13d5f78410209f9fc50dfad1791a6
王铭顺 <553147551@qq.com>
2022-02-26 19:42:33 +0800

    数组扁平

--- 2022春招/手写题/数组扁平化.js
+++ 2022春招/手写题/数组扁平化.js
@@ -1,3 +1,22 @@
-const nums = [1, [2, [3, [4]]]]
+const arr = [1, 2, 3, 4, [1, 2, 3, [1, 2, 3, [1, 2, 3]]], 5, "string", {
+    name: "弹铁蛋同学"
+}];
+// 递归调用
+function myFlat(arr) {
+    let res = []
+    fn(arr)
+    function fn(arr) {
+        arr.forEach((item) => {
+            if (Array.isArray(item)) {
+                fn(item)
+            } else {
+                res.push(item)
+            }
+        })
+    }
 
-console.log(nums.flat(3));
+    return res
+}
+
+
+console.log(myFlat(arr));
