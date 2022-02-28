146500dc3bc1225de1d57f80c3f354b54b9d3e3f
王铭顺 <553147551@qq.com>
2022-02-28 12:06:59 +0800

    eventloop

--- 2022春招/面试收录/js/event.js
+++ 2022春招/面试收录/js/event.js
@@ -1,7 +1,35 @@
-async function test() {
-  console.log('123');
+// a = Promise{100}
+// b = 100
+// 进入微任务队列
+// script start 1
+// async1 start
+// async2
+// script end
+// Promise{100}
+// 100
+// async1 end
+
+async function fn() {
   return 100
 }
 
-const f1 = test()
-f1.then(res => console.log(res))
+(async function () {
+  const a = fn()
+  const b = await fn()
+  console.log(a);
+  console.log(b);
+})()
+
+async function async1() {
+  console.log('async1 start');
+  await async2()
+  console.log('async1 end');
+}
+
+async function async2() {
+  console.log('async2');
+}
+
+console.log('script start');
+async1()
+console.log('script end');
--- 2022春招/面试收录/js/test.js
+++ 2022春招/面试收录/js/test.js
@@ -47,8 +47,23 @@
 //   console.log(b, '2');
 // })()
 // console.log(123);
-
-async function test() {
-  return 123
+async function async1() {
+  console.log('async1 start');
+  await async2();
+  console.log('async1 end');
 }
-console.log(test()); // Promise { 123 }
+async function async2() {
+  console.log('async2');
+}
+console.log('script start');
+setTimeout(function () {
+  console.log('setTimeout');
+}, 0);
+async1();
+new Promise(function (resolve) {
+  console.log('promise1');
+  resolve();
+}).then(function () {
+  console.log('promise2');
+});
+console.log('script end');
