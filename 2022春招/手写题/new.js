8d56fc04d1fd9ba5fa3752d893ef774791110bf6
王铭顺 <553147551@qq.com>
2022-03-05 20:00:35 +0800

    jj

---
+++ 2022春招/手写题/new.js
@@ -0,0 +1,19 @@
+function Person(name, age) {
+    this.name = name
+    this.age = age
+    return {}
+}
+let p = new Person('布兰', 12)
+console.log(p) // Person { name: '布兰', age: 12 }
+function myNew() {
+    let obj = {}
+    let fn = [].shift.call(arguments)
+    obj.__proto__ = fn.prototype
+    let result = fn.apply(obj, arguments)
+    console.log(result);
+    return result instanceof Object ? result : obj
+}
+let p2 = myNew(Person, 1, 2)
+console.log(p2);
+
+// 需要判断函数的返回值类型，如果是值类型，返回创建的对象。如果是引用类型，就返回这个引用类型的对象。
