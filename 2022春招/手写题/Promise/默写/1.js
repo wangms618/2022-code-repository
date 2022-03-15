22c1707d17815578a6b85507d8a3224638f403ed
王铭顺 <553147551@qq.com>
2022-03-15 11:53:16 +0800

    1

---
+++ 2022春招/手写题/Promise/默写/1.js
@@ -0,0 +1,34 @@
+class myPromise {
+    constructor(executor) {
+        this.state = 'pending'
+        this.value = null
+        this.reason = null
+        this.resolve = this.resolve.bind(this)
+        this.reject = this.reject.bind(this)
+        executor(this.resolve, this.reject)
+    }
+
+    resolve(value) {
+        if (this.state !== 'pending') return
+        this.value = value
+        this.state = 'fulfilled'
+    }
+
+    reject(reason) {
+        if (this.state !== 'pending') return
+        this.state = 'rejected'
+        this.reason = reason
+    }
+}
+const test1 = new myPromise((resolve, reject) => {
+    resolve('成功')
+    resolve('再次成功')
+})
+console.log(test1)
+// myPromise {value: "成功", reason: null, state: "fulfilled", resolve: ƒ, reject: ƒ}
+
+const test2 = new myPromise((resolve, reject) => {
+    reject('失败')
+    resolve('成功')
+})
+console.log(test2)
