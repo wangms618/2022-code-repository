6d6072f58dd0975e4177cc0e3297bd8c1d7e5afc
王铭顺 <553147551@qq.com>
2022-03-16 14:52:54 +0800

    1

--- 2022春招/手写题/Promise/文章练习/3.js
+++ 2022春招/手写题/Promise/文章练习/3.js
@@ -1,58 +1,64 @@
 const PENDING = 'pending'
 const FULFILLED = 'fulfilled'
 const REJECTED = 'rejected'
-// 三种状态
-
 class myPromise {
-    // 传入一个函数
     constructor(executor) {
-        // 成功值
         this.value = null
-        // 失败的原因
         this.reason = null
-        // 初始状态
         this.state = PENDING
-        // 初始化this指向
-        // bind会返回一个指向当前环境this的函数
+
+        // 保存then中的成功回调
+        this.onFulfilledCallbacks = []
+        // 保存then中的失败回调
+        this.onRejectedCallbacks = []
+
         this.resolve = this.resolve.bind(this)
         this.reject = this.reject.bind(this)
-        // 存放回调函数
-        this.onFulfilledCallbacks = []
-        this.onRejectedCallbacks = []
-        // 调用形式，new Promise((resolve, reject) => {})
-        // executor是一个回调函数，接收两个参数
-        executor(this.resolve, this.reject)
+        try {
+            executor(this.resolve, this.reject)
+        } catch (error) {
+            this.reject(error)
+        }
     }
-
     resolve(value) {
         if (this.state !== PENDING) return
         this.value = value
         this.state = FULFILLED
-        // 如果此时，已有回调在等待，直接执行
-        this.onFulfilledCallbacks.map(cb => cb(value))
-    }
 
+        // 调用onFulfilledCallbacks里保存的onFulfilled的回调
+        this.onFulfilledCallbacks.map(cb => cb(value))
+
+    }
     reject(reason) {
         if (this.state !== PENDING) return
         this.reason = reason
         this.state = REJECTED
+
+        // 调用onRejectedCallbacks里保存的then的onRejected回调
         this.onRejectedCallbacks.map(cb => cb(reason))
+
     }
-    then(fulfilled, rejected) {
-        fulfilled = typeof fulfilled === 'function' ? fulfilled : v => v
-        rejected = typeof rejected === 'function' ? rejected : err => {
+    then(onFulfilled, onRejected) {
+        // 保持指针指向
+        const that = this
+        onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v
+        onRejected = typeof onRejected === 'function' ? onRejected : err => {
             throw err
         }
-        if (this.state == FULFILLED) {
-            fulfilled(this.value)
-        } else if (this.state == REJECTED) {
-            rejected(this.reason)
-        } else { // 走到最后一步，表示此时，还没有执行resolve()或reject()，这时需将回调存起来
-            this.onFulfilledCallbacks.push(fulfilled)
-            this.onRejectedCallbacks.push(rejected)
+        if (this.state === FULFILLED) {
+            onFulfilled(this.value)
+        } else if (this.state === REJECTED) {
+            onRejected(this.reason)
+        } else { // 状态为pending时
+
+            // 保存回调函数 
+            that.onFulfilledCallbacks.push(onFulfilled)
+            that.onRejectedCallbacks.push(onRejected)
+
         }
     }
 }
+
 const test7 = new myPromise((resolve, reject) => {
     setTimeout(() => {
         resolve('成功')
