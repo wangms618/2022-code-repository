ed272d58fcbd475a6620435a8f9d2a672d916478
王铭顺 <553147551@qq.com>
2022-04-07 10:35:09 +0800

    拍平

--- 2022春招/手写题/手写2/10.js
+++ 2022春招/手写题/手写2/10.js
@@ -17,3 +17,13 @@ const arr = [1, 2, 3, 4, [1, 2, 3, [1, 2, 3, [1, 2, 3]]], 5, "string", {
     name: "弹铁蛋同学"
 }];
 console.log(myFalt(arr, 4));
+
+let res2 = JSON.stringify(arr).replace(/\[|\]/g, '')
+res2 = JSON.parse('[' + res2 + ']')
+
+
+const myFalt2 = (arr, depth) => {
+    return arr.reduce((pre, next) => {
+        return pre.concat((Array.isArray(next) && depth > 0) ? myFalt2(next, depth - 1) : next)
+    }, [])
+}
