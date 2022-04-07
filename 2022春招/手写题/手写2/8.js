c4e1f0ce0ea93fc83916e780633eab9516ecffc9
王铭顺 <553147551@qq.com>
2022-04-07 10:24:17 +0800

    浅拷贝

---
+++ 2022春招/手写题/手写2/8.js
@@ -0,0 +1,15 @@
+// function clone(obj) {
+//     if (typeof obj !== 'object' || obj === null) {
+//         return obj
+//     }
+//     let result = Array.isArray(obj) ? [] : {}
+//     for (let key in obj) {
+//         if (obj.hasOwnProperty(key)) {
+//             result[key] = obj[key]
+//         }
+//     }
+//     return result
+// }
+
+// let obj2 = Object.assign({},obj1)
+// let obj2 = {...obj1}
