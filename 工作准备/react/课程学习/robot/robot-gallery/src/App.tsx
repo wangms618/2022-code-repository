22007ce34b3ceaa173d591c4822ff4a08b067356
王铭顺 <553147551@qq.com>
2022-06-08 11:19:50 +0800

    css

--- 工作准备/react/课程学习/robot/robot-gallery/src/App.tsx
+++ 工作准备/react/课程学习/robot/robot-gallery/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "./assets/images/logo.svg";
 import styles from "./App.module.css";
 import robots from "./mockdata/robot.json";
 import Robot from "./components/Robot";
@@ -6,6 +7,10 @@ import Robot from "./components/Robot";
 function App() {
     return (
         <div className={styles.app}>
+            <div className={styles.appHeader}>
+                <img src={logo} alt="" className={styles.appLogo} />
+                <h1>啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦了</h1>
+            </div>
             <div className={styles.robotList}>
                 {robots.map(r => (
                     <Robot id={r.id} email={r.email} name={r.name} />
