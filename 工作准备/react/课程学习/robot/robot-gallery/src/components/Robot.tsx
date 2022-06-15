558ef21d709d9b319663f1616c4a7e795fe315fd
王铭顺 <553147551@qq.com>
2022-06-15 21:14:13 +0800

    context上下文

--- 工作准备/react/课程学习/robot/robot-gallery/src/components/Robot.tsx
+++ 工作准备/react/课程学习/robot/robot-gallery/src/components/Robot.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Robot.module.css";
+import { appContext } from "../index";
 interface RobotProps {
     id: number;
     name: string;
@@ -11,11 +12,18 @@ interface RobotProps {
 // type React.FC<P = {}> = React.FunctionComponent<P>，默认是一个空对象，P是泛型参数props
 const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
     return (
-        <div className={styles.cardContainer}>
-            <img src={`https://robohash.org/${id}`} alt="robot" />
-            <h2>{name}</h2>
-            <p>{email}</p>
-        </div>
+        <appContext.Consumer>
+            {value => {
+                return (
+                    <div className={styles.cardContainer}>
+                        <img src={`https://robohash.org/${id}`} alt="robot" />
+                        <h2>{name}</h2>
+                        <p>{email}</p>
+                        <p>作者：{value.username}</p>
+                    </div>
+                );
+            }}
+        </appContext.Consumer>
     );
 };
 
--- 工作准备/react/课程学习/robot/robot-gallery/src/index.tsx
+++ 工作准备/react/课程学习/robot/robot-gallery/src/index.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// Context上下文
+const defaultContextValue = {
+    username: "老鱼吃虾米",
+};
+
+export const appContext = React.createContext(defaultContextValue);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <appContext.Provider value={defaultContextValue}>
+            <App />
+        </appContext.Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
