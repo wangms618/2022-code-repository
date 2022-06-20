6259f2de889e074b35e71eccabd2a369dbde9751
王铭顺 <553147551@qq.com>
2022-06-20 11:33:52 +0800

    redyux

--- 工作准备/react/课程学习/react-travel/src/App.tsx
+++ 工作准备/react/课程学习/react-travel/src/App.tsx
@@ -7,21 +7,18 @@ function App() {
     return (
         <div className={styles.App}>
             <BrowserRouter>
-                {/* 页面切换switch */}
                 <Switch>
                     <Route exact path="/" component={HomePage} />
                     <Route path="/signIn" component={SignInPage} />
                     <Route path="/register" component={RegisterPage} />
-                    {/* 携带参数,格式：/detail/123 */}
                     <Route
                         path="/detail/:touristRouteId"
                         component={DetailPage}
                     />
                     <Route
-                        render={() => <h1>404 not found 页面不存在</h1>}
-                    ></Route>
+                        render={() => <h1>404 not found 页面去火星了 ！</h1>}
+                    />
                 </Switch>
-                {/* exact告诉route组件，有且仅能url的路径一模一样的时候才能执行，如果不加，/signIn路由下也可打开'/'下的页面 */}
             </BrowserRouter>
         </div>
     );
--- 工作准备/react/课程学习/react-travel/src/index.tsx
+++ 工作准备/react/课程学习/react-travel/src/index.tsx
@@ -1,14 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import "antd/dist/antd.min.css"; // or 'antd/dist/antd.less'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'antd/dist/antd.css';
 
-const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+
