a16df0ba0ade1c8a9c64298f8f764d4ddf5173a2
王铭顺 <553147551@qq.com>
2022-06-23 16:55:28 +0800

    文件数

---
+++ 工作准备/answer/node.js
@@ -0,0 +1,44 @@
+import {
+    readdir,
+    stat
+} from 'node:fs/promises';
+const fileList = {}
+// 传入文件列表和初始目录
+const search = async (files, basePath) => {
+    await files.forEach(async item => {
+        const data = await stat(`${basePath}/${item}`)
+        // 如果是目录，继续遍历
+        if (data.isDirectory()) {
+            const path = `${basePath}/${item}`
+            const file = await readdir(path)
+            await search(file, path)
+        }
+        // // 如果是文件,取到文件的后缀查看
+        if (data.isFile()) {
+            const type = item.split(".").pop()
+            if (fileList[type]) {
+                fileList[type] = ++fileList[type]
+            } else {
+                fileList[type] = 1
+            }
+        }
+    })
+}
+
+
+// 如果一个文件有"."说明不是目录
+const searchFileType = async () => {
+    try {
+        const files = await readdir("./")
+        // 等search执行完就可以得到fileList
+        await search(files, "./")
+        setTimeout(() => {
+            console.log(fileList);
+        }, 300)
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+searchFileType()
