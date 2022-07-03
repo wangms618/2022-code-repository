ed27d57782a76210cef105394e49db05f1973c8a
王铭顺 <553147551@qq.com>
2022-07-03 22:50:35 +0800

    observer

--- 工作准备/Mobx/TodoList.jsx
+++ 工作准备/Mobx/TodoList.jsx
@@ -1,4 +1,9 @@
-// observer包裹，
+// 如果只删除TodoView上的observer，保留TodoList组件上的observer，更新数据，TodoView依旧重新渲染，
+// 所以父组件使用observer，子组件也是被包裹在内的，会同样监听
+
+
+// observer包裹，如果禁用这里的observer，添加或删除列表项，列表不会更新，也就是修改了store的数据，组件TodoList不重新渲染，TodoView同理
+// 代码很好地证明了我们只需要更改数据，而不需要做更多的数据记录。MobX 会重新从 store 里的状态中自动派生并更新用户界面中相关的部分。
 const TodoList = observer(({ store }) => {
     const onNewTodo = () => {
         // addTodo添加listItem
@@ -7,7 +12,7 @@ const TodoList = observer(({ store }) => {
 
     return (
         <div>
-            {/* 展示 */}
+            {/* 展示，每次组件渲染都会调用它 */}
             {store.report}
             <ul>
                 {/* 数据渲染 */}
